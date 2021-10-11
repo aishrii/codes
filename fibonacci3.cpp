53ebf7bfc4e693c3a8bed50e02d521e5dd84f82f
Aishwarya Srivastava <aishrii.sri@gmail.com>
2021-10-11 15:35:17 +0530

    create fibonacci3.cpp

---
+++ fibonacci3.cpp
@@ -0,0 +1,32 @@
+//Member of fibonacci or not using recursive function
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t = 0, chk;
+    cin >> chk;
+    for(int i = 3; i <= chk; i++)
+    {
+        if(fib(i) == c)
+        {
+            t = 1;
+            break;
+        }
+        a = b;
+        b = c;
+    }
+    if(t == 1)
+    cout << "Member";
+    else
+    cout << "Not member";
+    return 0;
+}
+int fib(int n)
+{
+    if(n == 1)
+    return 0;
+    else if(n == 2)
+    return 1;
+    else
+    return fib(n - 1) + fib(n - 2);
+}
