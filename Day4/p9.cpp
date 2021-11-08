9232716c2ed17868abfd2c928ec551106d0dda13
Aishwarya Srivastava <aishrii.sri@gmail.com>
2021-11-08 16:14:04 +0530

    create p9.cpp

---
+++ Day4/p9.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main ()
+{
+    int n;
+    cin >> n;
+    int A[n];
+    for(int i = 0; i < n; i++)
+    {
+        cin >> A[i];
+    }
+    for(int i = 0; i < n; i = i + 2)
+    {
+        swap(A[i], A[i + 1]);
+    }
+    for(int i = 0; i < n; i++)
+    {
+        cout << A[i] << " ";
+    }
+    return 0;
+}
