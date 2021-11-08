9f3d33767742457b45cee3e0ba63fc7528560ff0
Aishwarya Srivastava <aishrii.sri@gmail.com>
2021-11-08 16:22:21 +0530

    create p9.cpp

--- Day4/p9.cpp
+++ Day4/p9.cpp
@@ -2,17 +2,22 @@
 using namespace std;
 int main ()
 {
-    int n;
+    int n, m;
     cin >> n;
     int A[n];
     for(int i = 0; i < n; i++)
     {
         cin >> A[i];
     }
-    for(int i = 0; i < n; i = i + 2)
+    if(n % 2 == 0)
+        m = n;
+    else
+        m = n - 1;
+    for(int i = 0; i < m; i = i + 2)
     {
         swap(A[i], A[i + 1]);
     }
+    
     for(int i = 0; i < n; i++)
     {
         cout << A[i] << " ";
