09a41cedcb565bfb0a050efd0d29abc236b3aee2
Aishwarya Srivastava <aishrii.sri@gmail.com>
2021-11-09 14:13:36 +0530

    create p14.cpp

---
+++ Day4/p14.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s, p;
+    cin >> s;
+    for(int i = 0; i < s.length(); i++)
+    {
+        p = s[i] + p;
+    }
+    cout << p;
+}
